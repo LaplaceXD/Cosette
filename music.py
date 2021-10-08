b3948337ab19db3b23d07fda68d645c6ba8b3ec3
Jonh Alexis <67273056+LaplaceXD@users.noreply.github.com>
2021-10-08 07:15:47 +0000

    CHANGED: Implementation details of timeout

--- music.py
+++ music.py
@@ -17,6 +17,8 @@ class Music(commands.Cog):
         self.has_joined = False
         self.paused = False
         self.queue = []
+        self.rechecks = 0
+        self.inactive = False
         
     def cog_unload(self, ctx):
         self.currently_playing = {}
@@ -24,30 +26,30 @@ class Music(commands.Cog):
         self.has_joined = False
         self.paused = False
         self.queue = []
+        self.rechecks = 0
+        self.inactive = False
     
     @commands.command(aliases=["j"])
     async def join(self, ctx):  
         if ctx.author.voice is None:
             await ctx.send("You're not in a voice channel!")
         else:
-            self.check_songs.start(ctx)
             if ctx.voice_client is None:
                 await ctx.author.voice.channel.connect()
             else:
                 await ctx.voice_client.move_to(ctx.author.voice.channel)
-            self.has_joined = True
             
     @commands.command(aliases=["d", "dc"], pass_context=True)
     async def disconnect(self, ctx):
         ctx.voice_client.stop()
-        self.check_songs.close(ctx)
-        self.check_if_playing.close(ctx)    
+        self.check_songs.cancel()
+        self.check_if_playing.cancel()    
 
         await ctx.voice_client.disconnect()
 
     @tasks.loop(minutes=3.0)
     async def check_songs(self, ctx):
-        if len(self.queue) == 0 and not bool(self.currently_playing) and self.has_joined:
+        if len(self.queue) == 0 and not bool(self.currently_playing) and self.inactive:
                 await ctx.send("Nangluod na ang bot.") 
                 await self.disconnect(ctx)
 
@@ -112,6 +114,7 @@ class Music(commands.Cog):
 
     @tasks.loop(seconds=5.0)
     async def check_if_playing(self, ctx):
+        self.rechecks += 1
         if not ctx.voice_client.is_playing() and not self.paused:
             self.song_started = False
             self.currently_playing = {} if len(self.queue) == 0 else self.queue.pop(0)
@@ -123,7 +126,14 @@ class Music(commands.Cog):
             return
         elif not bool(self.currently_playing):
             print("No songs in queue")
+            if self.rechecks == 12:
+                print("Disconnecting in two minutes.")
+                self.check_songs.start(ctx)
+                self.inactive = True
             return
+
+        self.rechecks = 0
+        self.inactive = False
 
         download_url = self.currently_playing["download_url"]
         display_url = self.currently_playing["url"]
