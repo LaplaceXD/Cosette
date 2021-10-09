7e22b076cc9aaf9f0506ece85ecaa79ea28d5769
Jonh Alexis <67273056+LaplaceXD@users.noreply.github.com>
2021-10-09 17:06:46 +0000

    ADDED: Simplified and verbose version of embed

--- app/music/song.py
+++ app/music/song.py
@@ -7,30 +7,36 @@ class MusicError(Exception):
     def __str__(self):
         return f"MUSIC ERROR: {self.message}" if self.message else f"MUSIC ERROR has been raised!"
 
-
 BOT_ICON_URL = "https://cdn.discordapp.com/attachments/797083893014462477/896312760084889600/unknown.png"
 
 class Music:
     def __init__(self, details: object):
         self.__details = details
 
-    def generate_embed(self, color=0xff0059, icon_url=BOT_ICON_URL):
+    def generate_embed(self, header: str, simplified=False, color=0xff0059, icon_url=BOT_ICON_URL):
         title = self.__details["title"]
         display_url = self.__details["url"]["display"]
         thumbnail = self.__details["thumbnail"]
-        channel = self.__details["channel"]
-        duration = self.__details["duration"]
-        likes = self.__details["like_count"]
-        dislikes = self.__details["dislike_count"]
 
-        return (Embed(title=title, url=display_url, color=color)
-            .set_author(name="▶️ Now playing!", icon_url=icon_url)
+        embed = (Embed(title=title, url=display_url, color=color)
             .set_thumbnail(url=thumbnail)
-            .add_field(name="📺 Channel", value=channel)
+            .set_footer(text="Made with love by Laplace ❤️"))
+
+        if header:
+            embed.set_author(name=header, icon_url=icon_url)
+        
+        if not simplified:
+            channel = self.__details["channel"]
+            duration = self.__details["duration"]
+            likes = self.__details["like_count"]
+            dislikes = self.__details["dislike_count"]
+
+            (embed.add_field(name="📺 Channel", value=channel)
             .add_field(name="🕒 Duration", value=duration, inline=False)
             .add_field(name="👍 Likes", value=likes)
-            .add_field(name="👎 Dislikes", value=dislikes)
-            .set_footer(text="Made with love by Laplace ❤️"))
+            .add_field(name="👎 Dislikes", value=dislikes))
+
+        return embed
     
     def get_details(self, format="simplified"):
         if format == "verbose":
