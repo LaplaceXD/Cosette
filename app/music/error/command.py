22e1ee336bc36e12e24c872b98c913f7c2fbbee5
Jonh Alexis <67273056+LaplaceXD@users.noreply.github.com>
2021-10-12 05:31:20 +0000

    [CHANGED] Implementation of different error levels to prevent strings

--- app/music/error/command.py
+++ app/music/error/command.py
@@ -5,13 +5,27 @@ from discord.ext import commands
 #  warning
 #  notice
 class MusicCommandError(commands.CommandError):
-    def __init__(self, title: str = "Command Error", description: str = "An error occured.", type: str = "warning", *args):
+    __levels = ["notice", "warning", "error"]
+
+    def __init__(self, title: str = "Command Error", description: str = "An error occured.", type: str = __levels[2], *args):
         super().__init__(title, description, type, *args)
         self.__error_details = {
             "title": title,
             "description": description
         }
         self.__error_type = type
+    
+    @property
+    def error(self):
+        return self.__levels[2]
+
+    @property
+    def warning(self):
+        return self.__levels[1]
+
+    @property
+    def notice(self):
+        return self.__levels[0] 
 
     @property
     def type(self):
